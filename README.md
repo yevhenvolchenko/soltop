b280f4498bc6baa9b424d89bc953d557a0536946
Yevhen Volchenko <eugene.volchenko@gmail.com>
2025-02-19 18:41:05 +0000

    docs: Add README and command descriptions

---
+++ README.md
@@ -0,0 +1,16 @@
+# It's like TOP, but for solana
+
+## Install
+First download and install go 1.23.6 from [https://go.dev/dl/](https://go.dev/dl/)
+To install soltop simply run
+```bash
+  go install github.com/yevhenvolchenko/soltop@latest
+```
+
+## Usage
+`soltop validators` outputs top 100 validators by stake list. The output is structured in a way that it can be used as a part of SQL compatible predicate.
+
+Example:
+```bash
+echo "SELECT * FROM cpu_load WHERE node IN $(go run main.go validators) - 1h"
+```
--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,13 +28,13 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "soltop",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "It's like TOP, but for solana",
+	// 	Long: `A longer description that spans multiple lines and likely contains
+	// examples and usage of using your application. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	// Cobra is a CLI library for Go that empowers applications.
+	// This application is a tool to generate the needed files
+	// to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
